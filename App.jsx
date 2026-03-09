c1743ffb6428062ebdb677dee2999529d738293f
ridpri <101707456+ridpri@users.noreply.github.com>
2026-03-09 13:25:43 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,7 @@
+import ProjectManagementDashboardPrototype from './ProjectManagementDashboardPrototype'
+
+function App() {
+  return <ProjectManagementDashboardPrototype />
+}
+
+export default App
